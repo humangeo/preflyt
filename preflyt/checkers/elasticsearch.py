f16de65078052ac5a3d88ddd4af584d8edacac54
Aru Sahni <aru@thehumangeo.com>
2016-06-26 14:58:13 -0400

    Initial checkin of Elasticsearch health checker.

---
+++ preflyt/checkers/elasticsearch.py
@@ -0,0 +1,41 @@
+"""Checks for Elasticsearch cluster health"""
+
+import json
+from urllib import request
+import urllib.error
+
+from preflyt.base import BaseChecker
+
+class ElasticsearchChecker(BaseChecker):
+    """Verify Elasticsearch is available and healthy"""
+
+    checker_name = "es"
+
+    def __init__(self, url, colors=None):
+        """Initialize the checker
+
+        :param name: The base URL of the Elasticsearch server
+        :param colors: Acceptable cluster health colors (other than Green)
+
+        """
+        super().__init__()
+        if not url.lower().startswith(("http://", "https://", "ftp://")):
+            url = "http://" + url
+        self._url = "{}/_cluster/health".format(url)
+        self._colors = {color.lower() for color in colors or []}
+        self._colors.add("green")
+
+    def check(self):
+        try:
+            with request.urlopen(self._url) as response:
+                body = response.read()
+                response = json.loads(body)
+                if response["status"] in self._colors:
+                    return True, "Cluster status is '{}'".format(response["status"])
+                return False, "Cluster status is '{}'".format(response["status"])
+        except urllib.error.HTTPError as httpe:
+            return False, "[{}] {}".format(httpe.code, httpe.reason)
+        except urllib.error.URLError as urle:
+            return False, urle.reason
+        except Exception as exc: # pylint: disable=broad-except
+            return False, "Unhandled error: {}".format(exc)
---
+++ tests/test_elasticsearch.py
@@ -0,0 +1,121 @@
+"""Test the escservice checker"""
+
+# pylint: disable=missing-docstring,protected-access
+
+from unittest.mock import MagicMock, patch
+import urllib.error
+
+from nose.tools import ok_, eq_
+
+from preflyt.checkers.elasticsearch import ElasticsearchChecker
+
+EXAMPLE_URL = "http://example.com"
+REQUEST_URL = "http://example.com/_cluster/health"
+RESPONSE_TEMPLATE = """
+{{
+  "cluster_name" : "testcluster",
+  "status" : "{color}",
+  "timed_out" : false,
+  "number_of_nodes" : 2,
+  "number_of_data_nodes" : 2,
+  "active_primary_shards" : 5,
+  "active_shards" : 10,
+  "relocating_shards" : 0,
+  "initializing_shards" : 0,
+  "unassigned_shards" : 0,
+  "delayed_unassigned_shards": 0,
+  "number_of_pending_tasks" : 0,
+  "number_of_in_flight_fetch": 0,
+  "task_max_waiting_in_queue_millis": 0,
+  "active_shards_percent_as_number": 100
+}}
+"""
+
+def test_init():
+    esc = ElasticsearchChecker(EXAMPLE_URL)
+    eq_(ElasticsearchChecker.checker_name, "es")
+    eq_(esc._url, REQUEST_URL)
+    eq_(esc._colors, {"green"})
+
+
+def test_init_colors():
+    esc = ElasticsearchChecker(EXAMPLE_URL, colors=["red", "YELLOW"])
+    eq_(ElasticsearchChecker.checker_name, "es")
+    eq_(esc._url, REQUEST_URL)
+    eq_(esc._colors, {"green", "red", "yellow"})
+
+def test_init_prefixless():
+    esc = ElasticsearchChecker("example.com")
+    eq_(esc._url, REQUEST_URL)
+
+@patch("urllib.request.urlopen")
+def test_check_healthy(urlopen):
+    esc = ElasticsearchChecker(EXAMPLE_URL)
+    response_mock = MagicMock()
+    response_mock.read.return_value = RESPONSE_TEMPLATE.format(color="green")
+    response_mock.__enter__.return_value = response_mock
+    urlopen.return_value = response_mock
+    result, message = esc.check()
+    urlopen.assert_called_with(REQUEST_URL)
+    ok_(result)
+    ok_("status is 'green'" in message, message)
+
+@patch("urllib.request.urlopen")
+def test_check_unhealthy(urlopen):
+    esc = ElasticsearchChecker(EXAMPLE_URL)
+    response_mock = MagicMock()
+    response_mock.read.return_value = RESPONSE_TEMPLATE.format(color="yellow")
+    response_mock.__enter__.return_value = response_mock
+    urlopen.return_value = response_mock
+    result, message = esc.check()
+    urlopen.assert_called_with(REQUEST_URL)
+    ok_(not result)
+    ok_("status is 'yellow'" in message, message)
+
+@patch("urllib.request.urlopen")
+def test_check_additional_color(urlopen):
+    esc = ElasticsearchChecker(EXAMPLE_URL, colors=["yellow"])
+    response_mock = MagicMock()
+    response_mock.read.return_value = RESPONSE_TEMPLATE.format(color="yellow")
+    response_mock.__enter__.return_value = response_mock
+    urlopen.return_value = response_mock
+    result, message = esc.check()
+    urlopen.assert_called_with(REQUEST_URL)
+    ok_(result)
+    ok_("status is 'yellow'" in message, message)
+
+@patch("urllib.request.urlopen")
+def test_check_additional_color_base(urlopen):
+    esc = ElasticsearchChecker(EXAMPLE_URL, colors=["yellow"])
+    response_mock = MagicMock()
+    response_mock.read.return_value = RESPONSE_TEMPLATE.format(color="green")
+    response_mock.__enter__.return_value = response_mock
+    urlopen.return_value = response_mock
+    result, message = esc.check()
+    urlopen.assert_called_with(REQUEST_URL)
+    ok_(result)
+    ok_("status is 'green'" in message, message)
+
+@patch("urllib.request.urlopen")
+def test_check_httperror(urlopen):
+    esc = ElasticsearchChecker(EXAMPLE_URL)
+    urlopen.side_effect = urllib.error.HTTPError(EXAMPLE_URL, 420, "HTTP ERROR HAPPENED", None, None)
+    result, message = esc.check()
+    ok_(not result)
+    ok_("[420]" in message)
+
+@patch("urllib.request.urlopen")
+def test_check_urlerror(urlopen):
+    esc = ElasticsearchChecker(EXAMPLE_URL)
+    urlopen.side_effect = urllib.error.URLError("URL ERROR HAPPENED")
+    result, message = esc.check()
+    ok_(not result)
+    ok_("URL ERROR HAPPENED" in message)
+
+@patch("urllib.request.urlopen")
+def test_check_unhandled(urlopen):
+    esc = ElasticsearchChecker(EXAMPLE_URL)
+    urlopen.side_effect = IOError("Whoops")
+    result, message = esc.check()
+    ok_(not result)
+    ok_("Unhandled error: " in message)
