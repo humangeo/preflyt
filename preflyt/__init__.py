282c311fb20206862e3da98d0b121b2210350c13
Aru Sahni <aru@thehumangeo.com>
2016-06-26 13:43:34 -0400

    Add verify method for exception-based flyt checking.

--- preflyt/__init__.py
+++ preflyt/__init__.py
@@ -48,6 +48,26 @@ def check(operations, loud=False):
             sys.stderr.write(" {}\n".format(pformat_check(success, operation, message)))
     return everything_ok, roll_call
 
+def verify(operations, loud=False):
+    """Check all the things and be assertive about it
+
+    :param operations: THe operations to check
+    :param loud: `True` if checkers should prettyprint their status to stderr. `False` otherwise.
+    :returns: The detailed execution log for the operations.
+
+    """
+    everything_ok, roll_call = check(operations, loud=loud)
+    if not everything_ok:
+        raise CheckFailedException(roll_call)
+    return roll_call
+
 class CheckerNotFoundError(Exception):
     """Couldn't find the checker."""
     pass
+
+class CheckFailedException(Exception):
+    """One or more of the system checks failed"""
+
+    def __init__(self, checks):
+        super().__init__()
+        self.checks = checks
--- tests/test_init.py
+++ tests/test_init.py
@@ -1,4 +1,7 @@
 """Test the checkers"""
+
+#pylint: disable=missing-docstring
+
 import os
 from unittest.mock import call, Mock, patch
 
@@ -24,7 +27,7 @@ def test_load_checkers(iter_modules):
         loader.find_module.assert_called_once_with(name)
         loader.find_module.return_value.load_module.assert_called_once_with(name)
 
-def test_everything():
+def test_check_success():
     """Test the check method."""
     good, results = preflyt.check(CHECKERS)
     ok_(good, results)
@@ -32,13 +35,30 @@ def test_everything():
     for field_name in ('check', 'success', 'message'):
         ok_(field_name in results[0])
 
-def test_everything_failure():
+def test_check_failure():
     """Test the check method."""
     good, results = preflyt.check(BAD_CHECKERS)
     ok_(not good, results)
     eq_(len(results), 1)
     for field_name in ('check', 'success', 'message'):
         ok_(field_name in results[0])
+
+def test_verify_success():
+    results = preflyt.verify(CHECKERS)
+    eq_(len(results), 1)
+    for field_name in ('check', 'success', 'message'):
+        ok_(field_name in results[0])
+
+def test_verify_failure():
+    raised = False
+    try:
+        preflyt.verify(BAD_CHECKERS)
+    except preflyt.CheckFailedException as cfe:
+        raised = True
+        eq_(len(cfe.checks), 1)
+        for field_name in ('check', 'success', 'message'):
+            ok_(field_name in cfe.checks[0])
+    ok_(raised)
 
 @patch("preflyt.load_checkers")
 def test_load_called(load_checkers):
@@ -54,7 +74,7 @@ def test_load_called(load_checkers):
 @patch("sys.stderr.write")
 @patch("preflyt.pformat_check")
 def test_everything_loud(pformat, stderr):
-    """Eat up teh stderr stuff."""
+    """Eat up the stderr stuff."""
     pformat.return_value = "item"
     preflyt.check(CHECKERS, loud=True)
     stderr.assert_has_calls([
